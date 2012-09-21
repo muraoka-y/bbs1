155362c919c5c494d9a5c449314ca982fa4d7a11
muraoka-y <muraoka-y@klab.com>
2012-09-21 03:13:28 -0700

    Update bbs2.php

--- bbs2.php
+++ bbs2.php
@@ -27,92 +27,48 @@ try {
 var_dump($result);
 }
 ?>
+<!DOCTYPE html>
 <html>
 <head>
-掲示板
-style type="text/css">
-
-.button {
-   border-top: 1px solid #3225e6;
-   background: #e6e617;
-   background: -webkit-gradient(linear, left top, left bottom, from(#e01d1d), to(#e6e617));
-   background: -webkit-linear-gradient(top, #e01d1d, #e6e617);
-   background: -moz-linear-gradient(top, #e01d1d, #e6e617);
-   background: -ms-linear-gradient(top, #e01d1d, #e6e617);
-   background: -o-linear-gradient(top, #e01d1d, #e6e617);
-   padding: 12.5px 25px;
-   -webkit-border-radius: 12px;
-   -moz-border-radius: 12px;
-   border-radius: 12px;
-   -webkit-box-shadow: rgba(0,0,0,1) 0 1px 0;
-   -moz-box-shadow: rgba(0,0,0,1) 0 1px 0;
-   box-shadow: rgba(0,0,0,1) 0 1px 0;
-   text-shadow: rgba(0,0,0,.4) 0 1px 0;
-   color: white;
-   font-size: 20px;
-   font-family: Georgia, serif;
-   text-decoration: none;
-   vertical-align: middle;
-   }
-.button:hover 
-{
-     border-top: 1px solid #3225e6;
-   background: #2dc5e0;
-   background: -webkit-gradient(linear, left top, left bottom, from(#231dde), to(#2dc5e0));
-   background: -webkit-linear-gradient(top, #231dde, #2dc5e0);
-   background: -moz-linear-gradient(top, #231dde, #2dc5e0);
-   background: -ms-linear-gradient(top, #231dde, #2dc5e0);
-   background: -o-linear-gradient(top, #231dde, #2dc5e0);
-   padding: 12.5px 25px;
-   -webkit-border-radius: 12px;
-   -moz-border-radius: 12px;
-   border-radius: 12px;
-   
-   }
-.button:active 
-{
-     border-top: 1px solid #3225e6;
-   background: #dede2f;
-   background: -webkit-gradient(linear, left top, left bottom, from(#1fde26), to(#dede2f));
-   background: -webkit-linear-gradient(top, #1fde26, #dede2f);
-   background: -moz-linear-gradient(top, #1fde26, #dede2f);
-   background: -ms-linear-gradient(top, #1fde26, #dede2f);
-   background: -o-linear-gradient(top, #1fde26, #dede2f);
-   padding: 12.5px 25px;
-   -webkit-border-radius: 12px;
-   -moz-border-radius: 12px;
-   border-radius: 12px;
-   
-   }
-</style>
+  <title>
+    BBS
+  </title>
+<link rel="stylesheet" href="bbs.css">
 </head>
 <body>
-<form action="bbs2.php" method="POST" name="f1" >
-<br>  
-<br>
-		1-Name
-	<br>
-		<input type="text"  name="name"  id="name" size="50"  placeholder="例；aaaa">
-	<br>	
-        投稿者のお名前を記入してください
-    <br>
-    2-Mail Address
-    <br>
-	<input type="text"  name="address"  id="address" size="50"  placeholder="例；xxx.klab.com">
-    <br>
-    3-title
-    <br>
-    <input type="text"  name="title"  id="title" size="50" >
-    <br>
-    4-TEXT
-    <br>
-	<textarea id="body" name="body" cols="100" rows="10" ></textarea>
-    <br>
-    <br>
-    <p><input type="submit" value="Post" class=button></p>
-    <p><input type="reset" value="Reset" class=button></p>
-    <br>
-    投稿
+  <h1>BBS</h1>
+  <img src="bahamut.jpeg" width="100" height="100" alt="bahamut" style="float:right;">
+<form action="bbs2.php" method="POST" name="f1" enctype="multipart/form-data">
+
+<div id="header"> Write your information </div>
+  <div class="form">
+    <label for="name">1-Name:</label> 
+    <input type="text"  name="name"  id="name" size="50"  placeholder="例；aaaa"></p></div>
+    <span class="point"><p style="color:red; solid #f00">投稿者のお名前を記入してください</p></span>
+    
+
+  <div class="form">
+    <label for="address">2-Adress:</label> 
+    <input type="text"  name="address"  id="address" size="50"  placeholder="例；xxx.klab.com"></p>
+  </div>
+  
+  <div class="form">
+    <label for="title">3-title:</label> 
+    <input type="text"  name="title"  id="title" size="50" ></p>
+  </div>
+   
+  <div class="form">
+    <label for="body">4-text:</label> 
+    <textarea id="body" name="body" cols="100" rows="10" ></textarea></p>
+  </div>
+
+  <div class="form">
+    <p><input type="file" name="picture" ></p>
+    <p><input type="submit" value="Post" class=button ></p>
+    <p><input type="reset" value="Reset" class=button ></p>
+   </div> 
+    
+    <div id="main"> Contribution</div>
     <?php
 $query = 'SELECT * FROM post ORDER BY posted_at DESC';
  foreach ($pdo->query($query) as $row) { //DBよりデータ取得
